4a4f3ce82f76aec236d6408a73373dc94d2d60f4
ANAND9301 <itsanandshrivastava@gmail.com>
2026-07-22 16:47:16 +0530

    data Ingestion done

--- setup.py
+++ setup.py
@@ -1,28 +1,24 @@
-from setuptools import setup, find_packages
-from typing import List 
+from setuptools import find_packages, setup 
+from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPHEN_DOT = '-e .'
 
-def get_requirements(file_path: str)->List[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+def get_requirements(file_path : str)->List[str] :
+    requirements = []
+    with open(file_path) as file_obj :
+        requirements = file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPHEN_DOT in requirements :
+            requirements.remove(HYPHEN_DOT)
 
     return requirements
 
-
 setup(
-    name='IPL-prediction',
-    version='0.0.1',
-    author='kunal',
-    author_email='kunalchopde98@gmail.com',
-    install_requires=get_requirements('requirements.txt'),
-    packages=find_packages()
+    name = "IPLPRED",
+    version = "0.0.1",
+    author = "anand",
+    author_email= "itsanandshrivastava@gmail.com",
+    install_requires = get_requirements("requirements.txt"),
+    packages = find_packages()
+
 )
-
-
